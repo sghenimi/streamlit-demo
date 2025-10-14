d7af382e1db883bb10b9ff8d359aab6899851a77
Soufiane Ghenimi <Soufiane@sgh-mac-1.home>
2025-10-14 20:11:25 +0200

    use diagram and map

--- uber_pickups.py
+++ uber_pickups.py
@@ -22,3 +22,15 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
+
+st.subheader("Raw Data")
+st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
