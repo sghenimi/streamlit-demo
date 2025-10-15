895f99dea770e6260bcb0dc41d5b4b3be8f624bc
Soufiane Ghenimi <Soufiane@sgh-mac-1.home>
2025-10-15 15:29:29 +0200

    add multi-pages

--- uber_pickups.py
+++ uber_pickups.py
@@ -23,14 +23,15 @@ data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
 
-st.subheader("Raw Data")
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
-hour_to_filter = 17
+hour_to_filter = st.slider('hour', 0, 23, 7)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
